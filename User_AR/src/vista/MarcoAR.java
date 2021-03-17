4a16039e7752d358c6f0917cd0de963ab368273b
DalessandroBautista <dalessandrobau@gmail.com>
2021-03-17 02:19:38 -0300

    cambio nombre de carpeta

---
+++ User_AR/src/vista/MarcoAR.java
@@ -0,0 +1,19 @@
+package vista;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+
+
+public class MarcoAR {
+
+	//public static void main(String[] args) {
+	public MarcoAR() {
+		MarcoTextoAR mimarco=new MarcoTextoAR();
+		mimarco.setVisible(true);
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
